d9a16dff2cb220c2b9ca6572ba4685e6382c06a1
ErnestoGalindo1139 <galindohernandezernesto@gmail.com>
2024-03-15 18:46:44 -0700

    Se avanzo en el apartado de nosotros

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -3,13 +3,14 @@ export const Nosotros = () => {
     return (
         <>
             <div className="color2">
-                <div className="ml-[5rem] py-[6rem] grid grid-cols-2 items-center justify-items-center gap-[4rem] w-3/4">
+                <div className="ml-[5rem] py-[6rem] grid grid-cols-2 items-center justify-items-start">
                     <img 
                         src="./Logos/nosotros1.jpg"
                         alt="nosotros1.jpg"
+                        className="w-11/12"
                     />
 
-                    <div>
+                    <div className="mr-[5rem]">
                         <h4 className="text-[#6b7280] font-bold text-[1.3rem] mb-[1rem]">• ACERCA DE NOSOTROS</h4>
                         <p className="text-white text-[1.8rem] text-justify">Adéntrate en el corazón de Sinaloa, donde la tradición y el sabor se fusionan para crear una experiencia única. Desde sus paisajes hasta su cocina, cada aspecto te sumerge en la autenticidad de esta tierra.</p>
                     </div>
